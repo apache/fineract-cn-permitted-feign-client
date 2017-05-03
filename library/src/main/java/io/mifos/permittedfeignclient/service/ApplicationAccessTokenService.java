44a4105a9b209126f7e1d196105d026d3e0ce258
myrle-krantz <mkrantz@mifos.org>
2017-05-03 23:56:33 +0200

    Initial commit.

---
+++ library/src/main/java/io/mifos/permittedfeignclient/service/ApplicationAccessTokenService.java
@@ -0,0 +1,143 @@
+/*
+ * Copyright 2017 The Mifos Initiative.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.mifos.permittedfeignclient.service;
+
+import io.mifos.anubis.config.TenantSignatureRepository;
+import io.mifos.anubis.security.AmitAuthenticationException;
+import io.mifos.anubis.token.TenantRefreshTokenSerializer;
+import io.mifos.anubis.token.TokenSerializationResult;
+import io.mifos.core.api.util.UserContextHolder;
+import io.mifos.core.lang.ApplicationName;
+import io.mifos.core.lang.TenantContextHolder;
+import io.mifos.core.lang.security.RsaKeyPairFactory;
+import io.mifos.identity.api.v1.client.IdentityManager;
+import io.mifos.identity.api.v1.domain.Authentication;
+import net.jodah.expiringmap.ExpirationPolicy;
+import net.jodah.expiringmap.ExpiringMap;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Nonnull;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author Myrle Krantz
+ */
+@Component
+public class ApplicationAccessTokenService {
+  private static final long REFRESH_TOKEN_LIFESPAN = TimeUnit.SECONDS.convert(1, TimeUnit.MINUTES);
+  private static class TokenCacheKey {
+    final String user;
+    final String tenant;
+    final String endpointSet;
+
+    private TokenCacheKey(final String user, final String tenant, final String endpointSet) {
+      this.user = user;
+      this.tenant = tenant;
+      this.endpointSet = endpointSet;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+      if (this == o) return true;
+      if (o == null || getClass() != o.getClass()) return false;
+      TokenCacheKey that = (TokenCacheKey) o;
+      return Objects.equals(user, that.user) &&
+              Objects.equals(tenant, that.tenant) &&
+              Objects.equals(endpointSet, that.endpointSet);
+    }
+
+    @Override
+    public int hashCode() {
+      return Objects.hash(user, tenant, endpointSet);
+    }
+
+    @Override
+    public String toString() {
+      return "TokenCacheKey{" +
+              "user='" + user + '\'' +
+              ", tenant='" + tenant + '\'' +
+              ", endpointSet='" + endpointSet + '\'' +
+              '}';
+    }
+  }
+
+  private final String applicationName;
+  private final TenantSignatureRepository tenantSignatureRepository;
+  private final IdentityManager identityManager;
+  private final TenantRefreshTokenSerializer tenantRefreshTokenSerializer;
+
+  private final Map<TokenCacheKey, TokenSerializationResult> refreshTokenCache;
+  private final Map<TokenCacheKey, Authentication> accessTokenCache;
+
+  @Autowired
+  public ApplicationAccessTokenService(
+          final @Nonnull ApplicationName applicationName,
+          final @Nonnull TenantSignatureRepository tenantSignatureRepository,
+          final @Nonnull IdentityManager identityManager,
+          final @Nonnull TenantRefreshTokenSerializer tenantRefreshTokenSerializer) {
+
+    this.applicationName = applicationName.toString();
+    this.tenantSignatureRepository = tenantSignatureRepository;
+    this.identityManager = identityManager;
+    this.tenantRefreshTokenSerializer = tenantRefreshTokenSerializer;
+
+    this.refreshTokenCache = ExpiringMap.builder()
+            .maxSize(300)
+            .expirationPolicy(ExpirationPolicy.CREATED)
+            .expiration(30, TimeUnit.SECONDS)
+            .entryLoader(tokenCacheKey -> this.createRefreshToken((TokenCacheKey)tokenCacheKey))
+            .build();
+    this.accessTokenCache = ExpiringMap.builder()
+            .maxSize(300)
+            .expirationPolicy(ExpirationPolicy.CREATED)
+            .expiration(30, TimeUnit.SECONDS)
+            .entryLoader(tokenCacheKey -> this.createAccessToken((TokenCacheKey)tokenCacheKey))
+            .build();
+  }
+
+  public String getAccessToken(final String endpointSetIdentifier) {
+    final TokenCacheKey tokenCacheKey
+            = new TokenCacheKey(UserContextHolder.checkedGetUser(), TenantContextHolder.checkedGetIdentifier(), endpointSetIdentifier);
+    final Authentication authentication = accessTokenCache.get(tokenCacheKey);
+    return authentication.getAccessToken();
+  }
+
+  private Authentication createAccessToken(final TokenCacheKey tokenCacheKey) {
+    final String refreshToken = refreshTokenCache.get(tokenCacheKey).getToken();
+    return identityManager.refresh(refreshToken);
+  }
+
+  private TokenSerializationResult createRefreshToken(final TokenCacheKey tokenCacheKey) {
+    final Optional<RsaKeyPairFactory.KeyPairHolder> optionalSigningKeyPair
+            = tenantSignatureRepository.getLatestApplicationSigningKeyPair();
+
+    final RsaKeyPairFactory.KeyPairHolder signingKeyPair = optionalSigningKeyPair.orElseThrow(AmitAuthenticationException::missingTenant);
+
+    final TenantRefreshTokenSerializer.Specification specification = new TenantRefreshTokenSerializer.Specification()
+            .setSourceApplication(applicationName)
+            .setUser(tokenCacheKey.user)
+            .setSecondsToLive(REFRESH_TOKEN_LIFESPAN)
+            .setPrivateKey(signingKeyPair.privateKey())
+            .setKeyTimestamp(signingKeyPair.getTimestamp())
+            .setEndpointSet(tokenCacheKey.endpointSet);
+
+    return tenantRefreshTokenSerializer.build(specification);
+  }
+}
---
+++ library/src/test/java/io/mifos/permittedfeignclient/service/ApplicationAccessTokenServiceTest.java
@@ -0,0 +1,62 @@
+package io.mifos.permittedfeignclient.service;
+
+import io.mifos.anubis.config.TenantSignatureRepository;
+import io.mifos.anubis.token.TenantRefreshTokenSerializer;
+import io.mifos.anubis.token.TokenSerializationResult;
+import io.mifos.core.api.context.AutoUserContext;
+import io.mifos.core.lang.ApplicationName;
+import io.mifos.core.lang.AutoTenantContext;
+import io.mifos.core.lang.security.RsaKeyPairFactory;
+import io.mifos.identity.api.v1.client.IdentityManager;
+import io.mifos.identity.api.v1.domain.Authentication;
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+/**
+ * @author Myrle Krantz
+ */
+public class ApplicationAccessTokenServiceTest {
+  private static final String APP_NAME = "app-v1";
+  private static final String BEARER_TOKEN_MOCK = "bearer token mock";
+  private static final String USER_NAME = "user";
+  private static final String TENANT_NAME = "tenant";
+  private static final String BEARER_INCOMING_ACCESS_TOKEN_MOCK = "bearer incoming access token mock";
+
+  @Test
+  public void testHappyCase() {
+    final ApplicationName applicationNameMock = Mockito.mock(ApplicationName.class);
+    Mockito.when(applicationNameMock.toString()).thenReturn(APP_NAME);
+
+    final TenantSignatureRepository tenantSignatureRepositoryMock = Mockito.mock(TenantSignatureRepository.class);
+    final Optional<RsaKeyPairFactory.KeyPairHolder> keyPair = Optional.of(RsaKeyPairFactory.createKeyPair());
+    Mockito.when(tenantSignatureRepositoryMock.getLatestApplicationSigningKeyPair()).thenReturn(keyPair);
+
+    final IdentityManager identityManagerMock = Mockito.mock(IdentityManager.class);
+    Mockito.when(identityManagerMock.refresh(Mockito.anyString()))
+            .thenReturn(new Authentication(BEARER_TOKEN_MOCK, "accesstokenexpiration", "refreshtokenexpiration", null));
+
+    final TenantRefreshTokenSerializer tenantRefreshTokenSerializerMock = Mockito.mock(TenantRefreshTokenSerializer.class);
+    Mockito.when(tenantRefreshTokenSerializerMock.build(Mockito.anyObject()))
+            .thenReturn(new TokenSerializationResult(BEARER_TOKEN_MOCK, LocalDateTime.now()));
+
+    final ApplicationAccessTokenService testSubject = new ApplicationAccessTokenService(
+            applicationNameMock,
+            tenantSignatureRepositoryMock,
+            identityManagerMock,
+            tenantRefreshTokenSerializerMock);
+
+    try (final AutoTenantContext ignored1 = new AutoTenantContext(TENANT_NAME)) {
+      try (final AutoUserContext ignored2 = new AutoUserContext(USER_NAME, BEARER_INCOMING_ACCESS_TOKEN_MOCK)) {
+        final String accessToken = testSubject.getAccessToken("blah");
+        Assert.assertEquals(BEARER_TOKEN_MOCK, accessToken);
+
+        final String accessTokenAgain = testSubject.getAccessToken("blah");
+        Assert.assertEquals(BEARER_TOKEN_MOCK, accessTokenAgain);
+      }
+    }
+  }
+}
